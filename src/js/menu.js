62ae1b79cbd99cf7186351f374a3121ec86b0298
opopopopopj <mixkovrigin@yandex.by>
2025-08-29 08:00:40 +0300

    menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -16,24 +16,12 @@
 		if ( document.body.classList.contains('menu-show') ) {
 
 			document.body.classList.remove('menu-show');
-			window.scrollTo(0,windowScroll);
-
-			setTimeout( () => document.documentElement.classList.remove('scroll-behavior-off'));
 
 			path.setAttribute('d','M3.334 5h13.333M3.334 10h13.333M3.334 15h13.333');
 
 		} else {
 
-			windowScroll = window.pageYOffset;
-
-			document.documentElement.classList.add('scroll-behavior-off');
-
-			setTimeout( () => {
-
-				document.body.classList.add('menu-show');
-				window.scrollTo(0,0);
-
-			});
+			document.body.classList.add('menu-show');
 
 			path.setAttribute('d','m5 5 10 10m0-10L5 15');
 
@@ -44,8 +32,6 @@
 	menu.addEventListener('click', event => {
 
 		if ( event.target.closest('a') && event.target.closest('a').href.includes('#') ) {
-
-			document.documentElement.classList.remove('scroll-behavior-off');
 
 			path.setAttribute('d','M3.334 5h13.333M3.334 10h13.333M3.334 15h13.333');
 
